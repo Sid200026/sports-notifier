e019856e9d297a0871126b85f6160189d4daf1dc
Sid200026 <siddharthsingharoy@gmail.com>
2020-01-09 02:41:10 +0530

    Completed football module

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,6 +1,8 @@
 import enum
 
+
 class FootballConsts(enum.Enum):
     baseURL = "https://www.fotmob.com"
-    ongoing = "/?show=ongoing&"
     noMatch = "No such team found. Please check if the team is currently playing or you have entered the name correctly"
+    apiConst = "https://api3.fotmob.com/matches?date={}&tz=19800000"
+    matchFinished = "Match Over"
--- scripts/football.py
+++ scripts/football.py
@@ -1,52 +1,63 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.chrome.options import Options
+from time import sleep
 from bs4 import BeautifulSoup
 import re
 from constants import FootballConsts
+from notification import notify
 import sys
+from datetime import datetime
+import requests
 
-def MatchDetails(url, matchDetails):
-    driver = webdriver.Chrome()
+
+def MatchDetails(url):
+    options = Options()
+    # Do not open the chrome browser
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
+    sleep(1)
     htmlSource = driver.page_source
     soup = BeautifulSoup(htmlSource, "lxml")
     team = []
-    teams = matchDetails.find_all("span", class_=re.compile("teamBlock"))
+    teams = soup.find_all("div", class_=re.compile("MatchHeaderTeamTitle"))
     for t in teams:
         team.append(t.string)
-    score = matchDetails.find("span", class_=re.compile("fm-match"))
-    time = matchDetails.find("span", class_=re.compile("matchTime"))
-    print(team)
-    print(score.string)
-    print(time.string)
-
-def getFootballMatch():
-    print("Enter team name (full name)")
-    teamName = input()
-    driver = webdriver.Chrome()
-    url = f"{FootballConsts.baseURL.value}{FootballConsts.ongoing.value}"
-    driver.get(url)
-    htmlSource = driver.page_source
-    soup = BeautifulSoup(htmlSource, "lxml")
-    matches = soup.find_all("a", class_=re.compile("livescore"))
-    foundTeam = False
-    matchURL = None
-    matchDetails = None
-    if matches is not None:
-        for match in matches:
-            if foundTeam:
-                break
-            teams = match.find_all("span", class_=re.compile("teamBlock"))
-            for team in teams:
-                if teamName.lower() in team.string.lower():
-                    matchURL = match.attrs['href']
-                    foundTeam = True
-                    matchDetails = match
-                    break
-    if not foundTeam:
-        print(FootballConsts.noMatch.value)
-        driver.close()
+    score = soup.find("div", class_=re.compile("LivescoreMatchScore"))
+    time = soup.find("div", class_=re.compile("LivescoreMatchTime"))
+    scoreline = f"{team[0]} {score.string.strip()} {team[1]}"
+    if time.string.strip().lower() in ['ft', 'full-time', 'fulltime']:
+        notify(scoreline, FootballConsts.matchFinished.value)
         sys.exit()
+    if time.string.strip().lower() in ["ht", "halftime", "half-time", "pause"]:
+        print("Half Time")
+        sleep(180)
+    # Do not get the seconds. Only the minutes
+    endindex = time.string.find(":")
+    timePassed = time.string[0:endindex] + " minutes"
     driver.quit()
-    url = f"{FootballConsts.baseURL.value}{matchURL}"
-    MatchDetails(url,matchDetails)
+    notify(scoreline, timePassed)
+    sleep(60)
+    MatchDetails(url)
+
+
+def fetchMatch():
+    d = datetime.today()
+    d = d.strftime("%Y%m%d")
+    url = FootballConsts.apiConst.value.format(d)
+    site = requests.get(url)
+    soup = BeautifulSoup(site.content, "lxml")
+    matches = soup.find_all("match")
+    print("Enter team name")
+    teamFound = False
+    teamName = input()
+    for match in matches:
+        team1 = match.attrs['ateam']
+        team2 = match.attrs['hteam']
+        if teamName in team1 or teamName in team2:
+            teamFound = True
+            url = f"{FootballConsts.baseURL.value}/livescores/{match.attrs['id']}"
+            MatchDetails(url)
+    if not teamFound:
+        print(FootballConsts.noMatch.value)
+        sys.exit()
