702904b84edc6ff449ceaa6501899aaa12356b9e
Sid200026 <siddharthsingharoy@gmail.com>
2020-01-09 02:42:07 +0530

    Added Linux support

---
+++ scripts/notification.py
@@ -0,0 +1,23 @@
+import os
+from sys import platform, exit
+
+
+def notify(title, text):
+    if platform == 'darwin':
+        notifyMACOS(title, text)
+    elif platform == 'linux':
+        notifyLinux(title, text)
+    else:
+        exit()
+
+
+def notifyMACOS(title, text):
+    os.system(f"""
+              osascript -e 'display notification "{text}" with title "{title}"'
+              """)
+
+
+def notifyLinux(title, text):
+    os.system(f"""
+                notify-send '{title}' '{text}' -u critical
+                """)
